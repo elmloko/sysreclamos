d94dec0a1897082d5b3c77f90e74f6173de3d30a
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-10 17:03:19 -0400

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Inicio Session AGBC</title>
+    <title>Inicio Session sysReclamos</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
