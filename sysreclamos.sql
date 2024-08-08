94294e89d8e05aeb88323cfaa8b4cc76c199b5a1
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-08-08 14:23:14 -0400

    Update sysreclamos.sql

--- sysreclamos.sql
+++ sysreclamos.sql
@@ -16,7 +16,7 @@
 
 
 -- Volcando estructura de base de datos para sysreclamos
-CREATE DATABASE IF NOT EXISTS `sysreclamos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `sysreclamos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `sysreclamos`;
 
 -- Volcando estructura para tabla sysreclamos.failed_jobs
