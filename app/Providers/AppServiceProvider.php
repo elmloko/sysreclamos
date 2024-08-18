adb5f35f618cd99340d307f7987e7159e3834d14
elmloko <56237010+elmloko@users.noreply.github.com>
2024-08-18 16:42:02 -0400

    Pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (?User $user) {
+            return $user !== null;
+        });
     }
 }
