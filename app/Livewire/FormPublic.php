21de6fda5c3d621f002d8ecebc3e61a8bc9689bf
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-01 14:44:17 -0400

    Update FormPublic.php

--- app/Livewire/FormPublic.php
+++ app/Livewire/FormPublic.php
@@ -28,6 +28,22 @@ class FormPublic extends Component
     {
         $this->validate();
 
+        // Obtener el último correlativo
+        $lastRecord = Suggestion::where('correlativo', 'like', 'SGR%')
+                        ->orderBy('correlativo', 'desc')
+                        ->first();
+
+        // Generar el nuevo correlativo
+        if ($lastRecord) {
+            $lastNumber = (int)substr($lastRecord->correlativo, 3); // Extrae el número
+            $newNumber = str_pad($lastNumber + 1, 4, '0', STR_PAD_LEFT); // Incrementa y formatea
+        } else {
+            $newNumber = '0001'; // Si no hay registros previos, empieza en 0001
+        }
+
+        $newCorrelativo = 'SGR' . $newNumber;
+
+        // Crear el nuevo registro con el correlativo y estado 'PUBLICADO'
         Suggestion::create([
             'fullName' => strtoupper($this->fullName),
             'address' => strtoupper($this->address),
@@ -36,8 +52,9 @@ class FormPublic extends Component
             'email' => $this->email,
             'phone' => strtoupper($this->phone),
             'description' => strtoupper($this->description),
+            'correlativo' => $newCorrelativo, // Asigna el correlativo
+            'estado' => 'PUBLICADO', // Asigna el estado
         ]);
-
 
         session()->flash('message', 'Reclamación enviada con éxito');
 
