724cf1869c258e316b98d19375737bca2a89a9b4
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-02 15:05:59 -0400

    Update estadisticas.blade.php

--- resources/views/livewire/estadisticas.blade.php
+++ resources/views/livewire/estadisticas.blade.php
@@ -1,68 +1,53 @@
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Conteo Diario</h3>
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>{{ $totalClaims }}</h3>
+                <p>Total Reclamos</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-        <div class="chart">
-            <canvas id="dailyCountsChart" style="height: 250px;"></canvas>
+    
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>{{ $totalComplaints }}</h3>
+                <p>Total Quejas</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>{{ $totalInformation }}</h3>
+                <p>Total Informaciones</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-person-add"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>{{ $totalSuggestions }}</h3>
+                <p>Total Sugerencias</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+            </div>
         </div>
     </div>
 </div>
-
-<script>
-    document.addEventListener('livewire:load', function () {
-        var ctx = document.getElementById('dailyCountsChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: {!! json_encode(array_keys($dailyCounts['claims'])) !!}, // Fechas
-                datasets: [
-                    {
-                        label: 'Claims',
-                        data: {!! json_encode(array_values($dailyCounts['claims'])) !!},
-                        borderColor: 'rgba(60,141,188,0.8)',
-                        backgroundColor: 'rgba(60,141,188,0.5)',
-                        fill: true,
-                    },
-                    {
-                        label: 'Complaints',
-                        data: {!! json_encode(array_values($dailyCounts['complaints'])) !!},
-                        borderColor: 'rgba(210, 214, 222, 1)',
-                        backgroundColor: 'rgba(210, 214, 222, 0.5)',
-                        fill: true,
-                    },
-                    {
-                        label: 'Information',
-                        data: {!! json_encode(array_values($dailyCounts['information'])) !!},
-                        borderColor: 'rgba(0, 166, 90, 1)',
-                        backgroundColor: 'rgba(0, 166, 90, 0.5)',
-                        fill: true,
-                    },
-                    {
-                        label: 'Suggestions',
-                        data: {!! json_encode(array_values($dailyCounts['suggestions'])) !!},
-                        borderColor: 'rgba(243, 156, 18, 1)',
-                        backgroundColor: 'rgba(243, 156, 18, 0.5)',
-                        fill: true,
-                    }
-                ]
-            },
-            options: {
-                maintainAspectRatio: false,
-                responsive: true,
-                scales: {
-                    x: {
-                        type: 'time',
-                        time: {
-                            unit: 'day',
-                            tooltipFormat: 'll',
-                        }
-                    },
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    });
-</script>
