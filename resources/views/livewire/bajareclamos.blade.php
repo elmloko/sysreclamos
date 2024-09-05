0b3acfc7c5baba562f19a7f57d3516a52a6733ec
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-05 11:00:18 -0400

    Update bajareclamos.blade.php

--- resources/views/livewire/bajareclamos.blade.php
+++ resources/views/livewire/bajareclamos.blade.php
@@ -3,7 +3,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Seguimiento de Reclamos AGBC</h1>
+                    <h1>Casos Cerrados Reclamos AGBC</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
