71e4f7f3b9eee7e2c15ef302df3a231bd6e1d39d
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-09-12 15:50:19 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@ URI     : https://colorlib.com
  -->
 
 <head>
-    <title>sysReclamos | AGBC</title>
+    <title>SIRECO | AGBC</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
