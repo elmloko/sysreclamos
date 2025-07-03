7ae792970cd1ee5373be7313a86d5288e35c2a73
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-07-03 11:55:17 -0400

    Revert "Fallo1"
    This reverts commit 3a57e5a0b6f8d1a62f1fa3d76401056221657359.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,9 +6,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
-use Illuminate\Support\Facades\Cookie;
-use Symfony\Component\HttpFoundation\Cookie as SymfonyCookie;
-use Illuminate\Support\Facades\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,26 +24,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Gate::define('viewPulse', function (?User $user) {
             return $user !== null;
-        });
-        // 🔒 Agrega HttpOnly a XSRF-TOKEN (opcional, si NO lo usas con JavaScript)
-        app('router')->middleware('web')->group(function () {
-            app('events')->listen('Illuminate\Routing\Events\RouteMatched', function () {
-                $token = csrf_token();
-    
-                Cookie::queue(
-                    new SymfonyCookie(
-                        'XSRF-TOKEN',
-                        $token,
-                        now()->addMinutes(config('session.lifetime')),
-                        '/',
-                        null,
-                        config('session.secure'),
-                        true, // HttpOnly
-                        false,
-                        'Lax'
-                    )
-                );
-            });
         });
     }
 }
