588f663d1ecc8869f1a73ba330179cb3343290f9
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-07-04 15:01:16 -0400

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-inline">
         {{ date('Y') }} Agencia Boliviana de Correos ©
-        <strong>TrackingBO</strong> Todos los derechos reservados.
+        <strong>SIRECO</strong> Todos los derechos reservados.
     </div>
     Todos los derechos reservados.<strong>sysReclamos</strong>
     <a href="mailto:mespinozarojas46@gmail.com" class="opacity-75" title="Marco Antonio Espinoza Rojas">Copyright © MAER
